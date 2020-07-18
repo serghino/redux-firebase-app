27397348142d26a146f85ddb0f3a3276593f07fd
Serghino <Serghino.1986@gmail.com>
2020-07-17 23:45:53 -0400

    feat: init version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,34 @@
 import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AuthGuardService } from './auth/auth-guard.service';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    {
+        path: '',
+        loadChildren: './ingreso-egreso/ingreso-egreso.module#IngresoEgresoModule',
+        canLoad: [ AuthGuardService ]
+    },
+    { path: '**', redirectTo: '' },
+
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+    imports: [
+        RouterModule.forRoot( routes )
+    ],
+    exports: [
+        RouterModule
+    ]
+
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
