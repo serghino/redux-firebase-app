5807c692eb568a138707294efd461c829f04f5b6
Serghino <Serghino.1986@gmail.com>
2020-07-25 22:26:44 -0400

    feat: stable version income expenses app

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,24 +4,55 @@ import 'firebase/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/firestore';
 
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import * as authActions from '../auth/auth.actions';
+import * as ingresoEgresoActions from '../ingreso-egreso/ingreso-egreso.actions';
+
 import { map } from 'rxjs/operators';
 import { Usuario } from '../models/usuario.model';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  userSubscription: Subscription;
+  private _user: Usuario;
+
+  get user() {
+    return this._user;
+  }
+
   constructor( public auth: AngularFireAuth,
-              private firestore: AngularFirestore) { }
+               private firestore: AngularFirestore,
+               private store: Store<AppState>) { }
 
   initAuthListener() {
 
     this.auth.authState.subscribe( fuser => {
-      console.log( fuser );
-      console.log( fuser?.uid );
-      console.log( fuser?.email );
-    })
+      if ( fuser ) {
+        // existe
+        this.userSubscription = this.firestore.doc(`${ fuser.uid }/usuario`).valueChanges()
+          .subscribe( (firestoreUser: any) => {
+          
+
+            const user = Usuario.fromFirebase( firestoreUser );
+            this._user = user;
+            this.store.dispatch( authActions.setUser({ user }) );
+            
+          })
+
+      } else {
+        // no existe
+        this._user = null;
+        this.userSubscription?.unsubscribe();
+        this.store.dispatch( authActions.unSetUser() );
+        this.store.dispatch( ingresoEgresoActions.unSetItems() );
+      }
+
+    });
 
   }
 
